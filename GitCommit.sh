80a09b6435805a790a1eb50b69e55892d0851f0a
Jun <wnsrl56@gmail.com>
2018-01-03 10:01:43 +0900

    add clean or add logic in GitCommit.sh

--- GitCommit.sh
+++ GitCommit.sh
@@ -1,19 +1,24 @@
 echo "Git add all and Show status"
 git add *
-STR=`git status`
-echo "$STR"
-read -p "Press enter to continue a commit......"
-echo -e "Select Git Comment Type\n1.long type 2.short type (default : 1)"
-read -p ">" type
-if [ $type == "2" ]
+git status
+stat=`git status`
+if [[ $stat == *"nothing to commit, working tree clean"* ]];
 then
-    echo "Write git comment"
-    read -p ">" msg
-    git commit -m "$msg"
+    echo "nothing to commit, working tree clean"
 else
-   git commit
+    read -p "Press enter to continue a commit......"
+        echo -e "Select Git Comment Type\n1.long type 2.short type (default : 1)"
+        read -p ">" type
+        if [ $type == "2" ]
+        then
+            echo "Write git comment"
+            read -p ">" msg
+            git commit -m "$msg"
+        else
+           git commit
+        fi
+        echo "finished......"
+        git log -5 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 fi
-echo "finished......"
-git log -5 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 read -p "Press enter to exit" out
 exit $out
