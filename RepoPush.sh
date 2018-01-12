f692102ad66323c750cef5b549cde319878c3ce1
jun <wnsrl5634@gmail.com>
2018-01-12 20:26:48 +0900

    add new feature to RepoPush.sh

--- RepoPush.sh
+++ RepoPush.sh
@@ -1,14 +1,20 @@
 #load child directories in current position
 #and do git pull && git push
-
 pointer=`pwd`
 DIR=`ls -d $pointer/*/`
 
+echo '###### GIT UPDATE & PUSH ######'
+echo -e 'Press Number What you want\n 1. update \n 2. Pull & Push\n'
+read -p ">" type
 for i in ${DIR};do
     echo "Entering directory to $i";
-    cd $i;
-    git pull && git push;
+    cd $i;    
+        if [ $type == "2" ]
+        then
+            git pull && git push;
+        else
+            git pull;
+        fi            
     cd ..;
 done
-
 read -p "Press enter to exit."
